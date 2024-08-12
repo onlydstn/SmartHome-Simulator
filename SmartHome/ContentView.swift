a6ea942ef475dd58610abe54f0157eefb2d99290
Dustin Nuzzo <dustin.nuzzo@icloud.com>
2024-08-12 11:47:06 +0200

    Initial Commit

---
+++ SmartHome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartHome
+//
+//  Created by Dustin Nuzzo on 12.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartHome/SmartHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Dustin Nuzzo on 12.08.24.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
